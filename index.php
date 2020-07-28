1401e729316638bca351fdef87e896cd3b165a09
Alexey Podvoyskiy <alexpa940@gmail.com>
2020-07-28 15:14:54 +0300

    Some codestyle

--- index.php
+++ index.php
@@ -14,68 +14,78 @@
 </head>
 
 <style>
-.form-group{
- margin-bottom: 15px;
-}
-label{
- margin-bottom: 15px;
-}
-input,
-input::-webkit-input-placeholder {
- font-size: 11px;
- padding-top: 3px;
-}
-.form-control {
- height: auto!important;
-padding: 8px 12px !important;
-}
-.input-group {
- box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.21)!important;
-}
-#button {
- border: 1px solid #ccc;
- margin-top: 28px;
- padding: 6px 12px;
- color: #666;
- text-shadow: 0 1px #fff;
- cursor: pointer;
- border-radius: 3px 3px;
- box-shadow: 0 1px #fff inset, 0 1px #ddd;
- background: #f5f5f5;
- background: -moz-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
- background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #f5f5f5), color-stop(100%, #eeeeee));
- background: -webkit-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
- background: -o-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
- background: -ms-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
- background: linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
- filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f5f5f5', endColorstr='#eeeeee', GradientType=0);
-}
-.main-form{
- margin-top: 30px;
- margin: 0 auto;
- max-width: 400px;
- padding: 10px 40px;
-background:#009edf;
-color: #FFF;
-text-shadow: none;
-box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.31);
-}
-span.input-group-addon i {
- color: #009edf;
- font-size: 17px;
-}
-.login-button{
- margin-top: 5px;
-}
+  .form-group{
+    margin-bottom: 15px;
+  }
+
+  label{
+    margin-bottom: 15px;
+  }
+
+  input,
+  input::-webkit-input-placeholder {
+    font-size: 11px;
+    padding-top: 3px;
+  }
+
+  .form-control {
+    height: auto!important;
+    padding: 8px 12px !important;
+  }
+
+  .input-group {
+    box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.21)!important;
+  }
+
+  #button {
+    border: 1px solid #ccc;
+    margin-top: 28px;
+    padding: 6px 12px;
+    color: #666;
+    text-shadow: 0 1px #fff;
+    cursor: pointer;
+    border-radius: 3px 3px;
+    box-shadow: 0 1px #fff inset, 0 1px #ddd;
+    background: #f5f5f5;
+    background: -moz-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #f5f5f5), color-stop(100%, #eeeeee));
+    background: -webkit-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
+    background: -o-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
+    background: -ms-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
+    background: linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f5f5f5', endColorstr='#eeeeee', GradientType=0);
+  }
+
+  .main-form{
+    margin-top: 30px;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 10px 40px;
+    background:#009edf;
+    color: #FFF;
+    text-shadow: none;
+    box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.31);
+  }
+
+  span.input-group-addon i {
+    color: #009edf;
+    font-size: 17px;
+  }
+
+  .login-button{
+    margin-top: 5px;
+  }
+
 </style>
 
 <body>
-<div class="container">
-<div class="row main-form">
-<body onload="document.registration.name.focus();">
-<h1>Регистрация</h1>
-<hr>
-<br>
+  <div class="container">
+  <div class="row main-form">
+  <body onload="document.registration.name.focus();">
+  
+  <h1>Регистрация</h1>
+  <hr>
+  <br>
 
     <form  class="" name='registration'  method="post" id="ajax_form" action="" >
 
@@ -131,7 +141,6 @@ span.input-group-addon i {
 
 
       <br>
-
       <hr>
 
       <div class="form-group ">
@@ -139,9 +148,12 @@ span.input-group-addon i {
       </div>
 
     </form>
+
     <div id="result_form"></div> 
+
     <br>
     <br>
+
     </div>
     </div>
     
